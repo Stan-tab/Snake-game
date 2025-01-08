3bce0daf32eb17ba42c7a843a68a72e3b8082596
Stan-tab <godofwarmafia2@gmail.com>
2025-01-08 20:38:22 +0500

    feat:rewrite logic

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
 const mainBox = document.querySelector(".mainBox");
-griding();
-const pixels = [...document.querySelectorAll("div *")];
-const snake = {
-    start: function() {
-        coordinates(16, 16);
-    },
-    head: function() {},
-    tail: function() {},
-}
+const size = 32;
+pixeling(size);
+let xPixel;
+let randomArr = [];
+const pixels = [...document.querySelectorAll("div > * > *")];
 
-function pixeling(size=32, parent, cls="y") {
+function pixeling(size=32) {
     for(let i = 0; i<size; i++) {
         const aciss = document.createElement("div");
-        aciss.classList = cls + +(i+1);
-        parent.appendChild(aciss);
+        aciss.classList = "y" + +(i+1);
+        mainBox.appendChild(aciss);
     };
-};
-
-function griding() {
-    pixeling(undefined , mainBox);
-    x_aciss = [...document.querySelectorAll(".mainBox > div")];
-    x_aciss.forEach(element => {
-        pixeling(undefined, element, "x");
+    let columnsArr = [...document.querySelectorAll("div > *")]
+    columnsArr.forEach(columns => {
+        for(let i = 0; i<size; i++) {
+            let col = columnsArr.indexOf(columns) + 1;
+            const aciss = document.createElement("div");
+            aciss.classList = "x" + +(i+1) + " y" + +col;
+            columns.appendChild(aciss);
+        };
     });
+    return;
 };
 
-function coordinates(x, y) {
-    yy = pixels[(32 * (y-1)) + (y-1)];
-    xx = pixels[(32 * (y-1)) + (y-1) + x];
-    return xx;
-};
+function coordinates(xCor, yCor) {
+    xPixel = document.querySelector(`.x${xCor}.y${yCor}`);
+    if (!xPixel) {
+        return console.error("lala");
+    }
+    return xPixel;
+}
+
+window.addEventListener("keypress", (e) => {
+    if(e.key.toLowerCase() == "d") { //right
+        if (randomArr.length >= 3) {
+            randomArr.shift().style.backgroundColor = "lightslategrey";
+        }
+        randomArr.push(coordinates(shortX, shortY))
+        coordinates(shortX, shortY).style.backgroundColor = "red";
+        shortX += 1;
+    } else if(e.key.toLowerCase() == "a") { //left
+    } else if(e.key.toLowerCase() == "w") { //up
+    } else if(e.key.toLowerCase() == "s") { //down
+    }
+});
+
+let shortX = 1;
+let shortY = 1;
+
+function FPS(fps = 30) {
+    setInterval(() => {
+        if(shortY == 33) {
+            shortY = 1;
+        }
+        if (randomArr.length >= 3) {
+            randomArr.shift().style.backgroundColor = "lightslategrey";
+        }
+        randomArr.push(coordinates(shortX, shortY))
+        coordinates(shortX, shortY).style.backgroundColor = "red";
+        shortX += 1;
+        if(shortX > 32) {
+            shortY += 1;
+            shortX = 1;
+        }
+        // console.log(randomArr);
+    }, 1000/fps)
+}
+
+FPS(10);
